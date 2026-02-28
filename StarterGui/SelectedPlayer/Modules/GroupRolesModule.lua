fe71d9fe02ff782508494bf54f973291a43c1fc1
nandoxts <nandoxts01@gmail.com>
2026-02-28 11:07:30 -0500

    feat: agregar módulo GroupRoles con IDs de usuarios desarrolladores

---
+++ StarterGui/SelectedPlayer/Modules/GroupRolesModule.lua
@@ -0,0 +1,13 @@
+-- GroupRolesModule
+local GroupRolesModule = {}
+
+GroupRolesModule.Group = {
+	DeveloperUserIds = {
+		5819550352,
+		1836329833,
+		8387751399,
+		4074563891,
+	},
+}
+
+return GroupRolesModule
